a3dc7198b5a8eff0e97e54b0ca6e6d10769d0437
FloraThallapaka <florathallapaka@gmail.com>
2025-03-25 17:23:55 +0530

    TriangleClassifier

--- src/Java_Tasks/Task_TriangleClassifier.java
+++ src/Java_Tasks/Task_TriangleClassifier.java
@@ -9,6 +9,15 @@ public class Task_TriangleClassifier {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
+        if(a<=0 || b<=0 || c<=0)
+        {
+            System.out.println("It's not a triangle");
+            System.exit(0);
+        }
+        if(a+b<=c || b+c<=a || c+a<=b) {
+            System.out.println("It's not a triangle");
+            System.exit(0);
+        }
         if (a == b && b == c)
             System.out.println("Given triangle is equilateral triangle");
         else if (a == b || b == c || c==a) {
