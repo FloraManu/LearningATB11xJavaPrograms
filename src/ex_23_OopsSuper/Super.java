e08765604c45a7715c16397c0af9f835e701bb93
FloraThallapaka <florathallapaka@gmail.com>
2025-04-08 05:34:24 +0530

    Super

---
+++ src/ex_23_OopsSuper/Super.java
@@ -0,0 +1,36 @@
+package ex_23_OopsSuper;
+
+public class Super {
+}
+class GOD{
+    void sound(){
+        System.out.println("God!");
+    }
+}
+
+class Animal extends GOD{
+    protected  String color  = "white";
+
+
+    void sound(){
+        System.out.println("Animal Sound!");
+        super.sound();
+    }
+
+}
+
+class Dog extends Animal{
+
+    private String color  = "Black";
+
+    Dog(){
+        //super();
+    }
+
+    void display(){
+        System.out.println(this.color);
+        System.out.println(super.color);
+        super.sound();
+    }
+
+}
